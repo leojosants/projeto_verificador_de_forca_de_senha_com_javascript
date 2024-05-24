7ad79afbf317dbb0d1652a31a65c3f0d9f2855e7
Leonardo Santos <leonardojosantos@gmail.com>
2024-05-24 14:49:58 -0300

    Adicionado funcionalidade de exibir e esconder senha

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Projeto desenvolvido em HTML, CSS e JS
+# Verificador de força de senha
+
+Verifica a força da senha através de regras como:
+
+- Conter letras minúsculas
+- Conter letras maiúsculas
+- Conter números
+- Conter caracteres especiais.
+
+## Tecnologias utilizadas
+
+- HTML
+- CSS
+- JavaScript
+
+### Interface
+
+<img 
+    src="./assets/images/project_presentation_screen.jpeg" 
+    style="border-radius: 5px" 
+    alt="Tela de apresentação do projeto"
+/>
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,53 @@
 const htmlElements = {
     DOM_passwordInput: document.querySelector('[data-id="password-input"]'),
     DOM_strengthIndicator: document.querySelector('[data-id="strength-indicator"]'),
-    DOM_strengthText: document.querySelector('[data-id="strength-text"]')
+    DOM_strengthText: document.querySelector('[data-id="strength-text"]'),
+    DOM_togglePassword: document.querySelector('[data-id-toggle-password]')
 };
 
-htmlElements.DOM_passwordInput.addEventListener('input',
+const events = {
+    click: 'click',
+    input: 'input'
+};
+
+const iconClasses = {
+    eye: 'fa-eye',
+    slash: 'fa-eye-slash'
+};
+
+const colors = {
+    color_e70b0b: '#e70b0b',
+    color_ffb74b: '#ffb74b',
+    color_fff176: '#fff176',
+    color_81c784: '#81c784'
+};
+
+const texts = {
+    password: 'password',
+    text: 'text',
+};
+
+htmlElements.DOM_togglePassword.addEventListener(events.click,
+    (event) => {
+        const eventTarget = event.target;
+        const type = htmlElements.DOM_passwordInput.type.includes(texts.password) ? texts.text : texts.password;
+        const passwordLength = htmlElements.DOM_passwordInput.value.length;
+
+        htmlElements.DOM_passwordInput.type = type;
+
+        if (passwordLength === 0) return
+
+        eventTarget.classList.toggle(iconClasses.eye);
+        eventTarget.classList.toggle(iconClasses.slash);
+    }
+);
+
+htmlElements.DOM_passwordInput.addEventListener(events.input,
     (event) => {
         const eventTargetValue = event.target.value;
-        const password = eventTargetValue;
+        const passwordValue = eventTargetValue;
+        const passwordLength = eventTargetValue.length;
+        const iconIncludesClassSlash = htmlElements.DOM_togglePassword.className.includes(iconClasses.slash);
 
         const strengths = {
             0: 'Muito fraca',
@@ -20,11 +60,11 @@ htmlElements.DOM_passwordInput.addEventListener('input',
         let score = 0;
 
         // requisitos
-        if (password.length >= 8) score++;
-        if (password.match(/[a-z]/)) score++;
-        if (password.match(/[A-Z]/)) score++;
-        if (password.match(/[0-9]/)) score++;
-        if (password.match(/[^a-zA-Z0-9]/)) score++;
+        if (passwordLength >= 8) score++;
+        if (passwordValue.match(/[a-z]/)) score++;
+        if (passwordValue.match(/[A-Z]/)) score++;
+        if (passwordValue.match(/[0-9]/)) score++;
+        if (passwordValue.match(/[^a-zA-Z0-9]/)) score++;
 
         const width = (score / 4) * 100;
 
@@ -32,19 +72,19 @@ htmlElements.DOM_passwordInput.addEventListener('input',
 
         switch (score) {
             case 1:
-                htmlElements.DOM_strengthIndicator.style.backgroundColor = '#e70b0b';
+                htmlElements.DOM_strengthIndicator.style.backgroundColor = colors.color_e70b0b;
                 break;
 
             case 2:
-                htmlElements.DOM_strengthIndicator.style.backgroundColor = '#ffb74b';
+                htmlElements.DOM_strengthIndicator.style.backgroundColor = colors.color_ffb74b;
                 break;
 
             case 3:
-                htmlElements.DOM_strengthIndicator.style.backgroundColor = '#fff176';
+                htmlElements.DOM_strengthIndicator.style.backgroundColor = colors.color_fff176;
                 break;
 
             case 4:
-                htmlElements.DOM_strengthIndicator.style.backgroundColor = '#81c784';
+                htmlElements.DOM_strengthIndicator.style.backgroundColor = colors.color_81c784;
                 break;
 
             default:
@@ -54,12 +94,16 @@ htmlElements.DOM_passwordInput.addEventListener('input',
 
         htmlElements.DOM_strengthIndicator.style.width = `${width}%`;
 
-        if (password.length > 0) {
+        if (passwordLength > 0) {
             htmlElements.DOM_strengthText.innerHTML = `Força ${strengths[score]}`;
-            console.log(score)
         }
         else {
             htmlElements.DOM_strengthText.innerHTML = 'Senha fraca';
+        }
+
+        if (passwordLength === 0 && iconIncludesClassSlash) {
+            htmlElements.DOM_togglePassword.classList.remove(iconClasses.slash);
+            htmlElements.DOM_togglePassword.classList.add(iconClasses.eye);
         }
     }
 );
